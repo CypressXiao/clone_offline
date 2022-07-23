84d2b7266d475b2bf66e3336c2f446ee86bfe539
hangge <598196583@qq.com>
2022-07-23 11:54:41 +0800

    distinct

--- shells/06 日志去重.sh
+++ shells/06 日志去重.sh
@@ -9,10 +9,11 @@
 
 DT=$(date -d'-1 days'  +%Y-%m-%d)
 # 用户指定上报日志的日志  2022-07-19
+# read  -p "请输入去重的日期: " DT
 if [ $1 ]; then
     DT=$1
 fi
-# read  -p "请输入日志类型: " LOGTYPE
+
 # 日志类型  这里使用默认的日志类型
 LOGTYPE=applogs
 
@@ -25,9 +26,24 @@ HDFSCNT=$(curl  http://windows:8081/api/getHdfsLogLines  -X POST   -d"{\"dt\":\"
 
 echo  "HDFS采集到的日志条数: $HDFSCNT"
 echo  "HDFS采集到的日志条数: $LOGCNT"
+#前提是 先创建一张表
+    # drop table if exists doe.tmp_raw_applog ;
+    #create external table if not exists doe.tmp_raw_applog(
+    #    line string
+    #)
+    # partitioned by (dt string)
+    # location '/doe/data/app_log/' ;
 
+# doe.tmp_raw_applog  加载原始日志数据
+hive -e "alter  table  doe.tmp_raw_applog   add  if not exists   partition (dt='${DT}') location  '/doe/data/app_log/${DT}'"
 if [  $HDFSCNT  -gt $LOGCNT ]; then
     echo  "需要去重"
+     # 指定输出压缩格式
+   sql2="set hive.exec.compress.output = true;
+       set mapred.output.compression.codec = org.apache.hadoop.io.compress.GzipCodec;
+       insert  overwrite  table  doe.tmp_raw_applog partition (dt='${DT}')
+       select  distinct line from doe.tmp_raw_applog  where  dt = '${DT}'"
+    hive -e  "${sql2}"
   else
     echo "不需要去重"
 fi
